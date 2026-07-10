3e645c6b7609943dc55ed9ec9270dccc5a8fddff
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-07-10 20:37:07 +0530

    add protected routes

--- src/layout/Dashboard.tsx
+++ src/layout/Dashboard.tsx
@@ -1,6 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { useAuthStore } from "@/store";
+import { Navigate, Outlet } from "react-router-dom";
 
 function Dashboard() {
+  // This will decide if the below protected route should be rendered or not
+  // We will check the store, if we have user data , then user is authenticated and allowed to see this page else not (we will redirect them to login page)
+  const { user } = useAuthStore();
+
+  if (!user) {
+    // user is not logged in
+    // redirect to login page
+    return <Navigate to="/auth/login" replace={true} />;
+  }
   return (
     <div>
       <h2>This is Dashboard</h2>
--- src/layout/NonAuth.tsx
+++ src/layout/NonAuth.tsx
@@ -1,6 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { useAuthStore } from "@/store";
+import { Navigate, Outlet } from "react-router-dom";
 
 function NonAuth() {
+  const { user } = useAuthStore();
+
+  if (user) {
+    // user is already logged in
+    // redirect to /  page
+    return <Navigate to="/" replace={true} />;
+  }
   return (
     <div>
       <h2>This is NonAuth</h2>
