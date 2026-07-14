7f4a2d38cdd42e790b7bf311f1c5c82da06b8df9
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-07-14 13:10:45 +0530

    add tailwind

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,22 @@
+import { useAuthStore } from "@/store";
+import { Typography } from "antd";
+
+const MAX_NAME_LENGTH = 10;
+const truncate = (str: string, len: number) => {
+  if (str.length > len) {
+    return str.substring(0, len - 3) + "...";
+  }
+  return str;
+};
+
 function HomePage() {
-  return <div>I am inside Admin Dashboard</div>;
+  const { user } = useAuthStore();
+
+  return (
+    <div>
+      <Typography.Title level={3}>Welcome, {truncate(user?.firstName || "", MAX_NAME_LENGTH)} 🙃</Typography.Title>
+    </div>
+  );
 }
 
 export default HomePage;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     environment: "jsdom",
     setupFiles: "./src/test/setup.ts",
