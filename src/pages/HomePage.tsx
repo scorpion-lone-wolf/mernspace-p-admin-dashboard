9a3496994fe58229b40c0c12b8971c7275a7b995
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-07-14 22:37:28 +0530

    add basic home page layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,68 @@
+import { BarChartIcon } from "@/components/icons/BarChartIcon";
+import BasketIcon from "@/components/icons/BasketIcon";
 import { useAuthStore } from "@/store";
-import { Typography } from "antd";
+import Icon from "@ant-design/icons";
+import { Button, Card, Col, List, Row, Skeleton, Space, Statistic, Tag, Typography } from "antd";
+import type { ComponentType } from "react";
+import { Link } from "react-router-dom";
 
-const MAX_NAME_LENGTH = 10;
-const truncate = (str: string, len: number) => {
-  if (str.length > len) {
-    return str.substring(0, len - 3) + "...";
-  }
-  return str;
+const { Title, Text } = Typography;
+const list = [
+  {
+    OrderSummary: "Peperoni, Margarita ...",
+    address: "Bandra, Mumbai",
+    amount: 1200,
+    status: "preparing",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+  {
+    OrderSummary: "Paneer, Chicken BBQ ...",
+    address: "Balurghat, West bengal",
+    amount: 2000,
+    status: "on the way",
+    loading: false,
+  },
+];
+interface CardTitleProps {
+  title: string;
+  PrefixIcon: ComponentType<unknown>;
+}
+
+const CardTitle = ({ title, PrefixIcon }: CardTitleProps) => {
+  return (
+    <Space>
+      <Icon component={PrefixIcon} />
+      {title}
+    </Space>
+  );
 };
 
 function HomePage() {
@@ -14,7 +70,59 @@ function HomePage() {
 
   return (
     <div>
-      <Typography.Title level={3}>Welcome, {truncate(user?.firstName || "", MAX_NAME_LENGTH)} 🙃</Typography.Title>
+      <Title level={4}>Welcome, {user?.firstName} 😀</Title>
+      <Row className="mt-4" gutter={[16, 16]}>
+        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+              <Card>
+                <Statistic title="Total orders" value={52} />
+              </Card>
+            </Col>
+            <Col span={12}>
+              <Card>
+                <Statistic title="Total sale" value={70000} precision={2} prefix="₹" />
+              </Card>
+            </Col>
+          </Row>
+          <Row gutter={[16, 16]} className="mt-4!">
+            <Col span={24}>
+              <Card title={<CardTitle title="Sales" PrefixIcon={BarChartIcon} />}></Card>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={12} xl={12}>
+          <Card title={<CardTitle title="Recent orders" PrefixIcon={BasketIcon} />}>
+            <List
+              className="demo-loadmore-list"
+              loading={false}
+              itemLayout="horizontal"
+              loadMore={true}
+              dataSource={list}
+              renderItem={(item) => (
+                <List.Item>
+                  <Skeleton avatar title={false} loading={item.loading} active>
+                    <List.Item.Meta title={<a href="https://ant.design">{item.OrderSummary}</a>} description={item.address} />
+                    <Row style={{ flex: 1 }} justify="space-between">
+                      <Col>
+                        <Text strong>₹{item.amount}</Text>
+                      </Col>
+                      <Col>
+                        <Tag color="volcano">{item.status}</Tag>
+                      </Col>
+                    </Row>
+                  </Skeleton>
+                </List.Item>
+              )}
+            />
+            <div style={{ marginTop: 20 }}>
+              <Button type="link">
+                <Link to="/orders">See all orders</Link>
+              </Button>
+            </div>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
