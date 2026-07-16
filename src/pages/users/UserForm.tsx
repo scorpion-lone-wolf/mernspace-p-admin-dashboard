cc170f56c045d335c583c90f8365775fbc1e8a64
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-07-16 16:45:23 +0530

    add user form sections

--- src/pages/users/UserForm.tsx
+++ src/pages/users/UserForm.tsx
@@ -1,27 +1,93 @@
-import { Card, Col, Form, Input, Row } from "antd";
+import { tenants } from "@/api/tenants.api";
+import type { Tenant } from "@/store";
+import { useQuery } from "@tanstack/react-query";
+import { Card, Col, Form, Input, Row, Select, Space } from "antd";
 
 function UserForm() {
+  const {
+    data: tenantData,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["tenants"],
+    queryFn: async () => {
+      return (await tenants()).data;
+    },
+  });
+  if (error) {
+    return <div>Something went wrong.</div>;
+  }
   return (
     <Row>
       <Col span={24}>
-        <Card title="Basic info">
-          <Row gutter={18}>
-            <Col span={12}>
-              <Form.Item name="firstName" label="First Name" rules={[{ required: true }, { type: "string", min: 6 }]}>
-                <Input placeholder="Enter your First Name" />
-              </Form.Item>
-            </Col>
-            <Col span={12}>
-              <Form.Item name="lastName" label="Last Name" rules={[{ required: true }, { type: "string", min: 6 }]}>
-                <Input placeholder="Enter your Last Name" />
-              </Form.Item>
-            </Col>
-          </Row>
+        <Space vertical className="w-full" size="large">
+          <Card title="Basic info">
+            <Row gutter={18}>
+              <Col span={12}>
+                <Form.Item name="firstName" label="First Name" rules={[{ required: true }, { type: "string", min: 6 }]}>
+                  <Input placeholder="Enter your First Name" size="medium" />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item name="lastName" label="Last Name" rules={[{ required: true }, { type: "string", min: 6 }]}>
+                  <Input placeholder="Enter your Last Name" size="medium" />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item name="Email" label="Email" rules={[{ required: true }, { type: "email", min: 6 }]}>
+                  <Input placeholder="Enter your Email" size="medium" />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
 
-          {/* <Form.Item name="Email" label="Email" rules={[{ required: true }, { type: "email", min: 6 }]}>
-            <Input placeholder="Enter your Email" />
-          </Form.Item> */}
-        </Card>
+          <Card title="Security info">
+            <Row gutter={18}>
+              <Col span={12}>
+                <Form.Item name="password" label="Password" rules={[{ required: true }, { type: "string", min: 6 }]}>
+                  <Input placeholder="Add your Password" size="medium" type="password" />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item name="confirmPassword" label="Confirm Password" rules={[{ required: true }, { type: "string", min: 6 }]}>
+                  <Input placeholder="Confirm your Password" size="medium" type="password" />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
+          <Card title="Roles info">
+            <Row gutter={18}>
+              <Col span={12}>
+                <Form.Item name="role" label="Role" rules={[{ required: true }, { type: "string" }]}>
+                  <Select
+                    placeholder="Select Role"
+                    size="medium"
+                    allowClear
+                    onChange={() => {}}
+                    options={[
+                      { value: "ADMIN", label: "Admin" },
+                      { value: "MANAGER", label: "Manager" },
+                      { value: "CUSTOMER", label: "Customer" },
+                    ]}
+                  />
+                </Form.Item>
+              </Col>
+              <Col span={12}>
+                <Form.Item name="tenantId" label="Resturant" rules={[{ required: true }, { type: "string" }]}>
+                  <Select
+                    placeholder="Select Resturant"
+                    size="medium"
+                    allowClear
+                    onChange={() => {}}
+                    loading={isLoading}
+                    options={tenantData.data?.map((tenant: Tenant) => ({ value: tenant.id, label: tenant.name }))}
+                    listHeight={50}
+                  />
+                </Form.Item>
+              </Col>
+            </Row>
+          </Card>
+        </Space>
       </Col>
     </Row>
   );
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 
-interface Tenant {
+export interface Tenant {
   id: string;
   name: string;
   address: string;
