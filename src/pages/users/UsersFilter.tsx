fc20318d650197c34f6fc21f34b7cbb2e277a5b8
Pursotam Singh <pursotam.singh@codeclouds.com>
2026-07-20 14:38:20 +0530

    add ui

--- src/pages/users/UsersFilter.tsx
+++ src/pages/users/UsersFilter.tsx
@@ -7,43 +7,32 @@ function UsersFilter({ children }: Readonly<UserFilterProps>) {
   return (
     <Card>
       <Row gutter={[16, 16]} align="middle">
-        <Col xs={24} sm={24} md={12} lg={8}>
-          <Form.Item name="search" className="mb-0!">
-            <Input.Search placeholder="Search for user" allowClear />
-          </Form.Item>
+        <Col xs={24} md={16}>
+          <Row gutter={[16, 16]}>
+            <Col xs={24} sm={16} lg={10}>
+              <Form.Item name="search" className="mb-0!">
+                <Input.Search placeholder="Search for user" allowClear />
+              </Form.Item>
+            </Col>
+
+            <Col xs={24} sm={6} md={8} lg={6}>
+              <Form.Item name="role" className="mb-0!">
+                <Select
+                  placeholder="Role"
+                  allowClear
+                  className="w-full"
+                  options={[
+                    { value: "ADMIN", label: "Admin" },
+                    { value: "MANAGER", label: "Manager" },
+                    { value: "CUSTOMER", label: "Customer" },
+                  ]}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
         </Col>
 
-        <Col xs={24} sm={12} md={6} lg={3}>
-          <Form.Item name="role" className="mb-0!">
-            <Select
-              placeholder="Role"
-              className="w-full"
-              options={[
-                { value: "ADMIN", label: "Admin" },
-                { value: "MANAGER", label: "Manager" },
-                { value: "CUSTOMER", label: "Customer" },
-              ]}
-              allowClear
-            />
-          </Form.Item>
-        </Col>
-
-        {/* <Col xs={24} sm={12} md={6} lg={3}>
-          <Select
-            placeholder="Status"
-            className="w-full"
-            options={[
-              { value: "BANNED", label: "Banned" },
-              { value: "ACTIVE", label: "Active" },
-            ]}
-            allowClear
-            onChange={(selectedValue) => {
-              onFilterChange("UserStatusFilter", selectedValue);
-            }}
-          />
-        </Col> */}
-
-        <Col xs={24} lg={10}>
+        <Col xs={24} md={8} lg={8}>
           <Flex justify="end">{children}</Flex>
         </Col>
       </Row>
